4e3ceac603aa5fa3662b898802c9748599522f83
Jieun <zieun.na@gmail.com>
2020-09-14 21:47:11 +0900

    add:초기세팅 수정

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './Routes';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
+import "reset.scss";
 
 
 ReactDOM.render(
