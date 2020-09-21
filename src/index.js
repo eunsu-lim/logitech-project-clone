b26ff2d42157c83b00de636dd59109e33b4e3239
Jieun <zieun.na@gmail.com>
2020-09-21 11:44:54 +0900

    Refactor: 충돌 해결

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
-<<<<<<< HEAD
-import "../src/Styles/common.scss";
-import "../src/Styles/reset.scss";
-=======
 import "../src/Styles/reset.scss";
 import "../src/Styles/common.scss";
->>>>>>> master
 
 ReactDOM.render(
   <React.StrictMode>
