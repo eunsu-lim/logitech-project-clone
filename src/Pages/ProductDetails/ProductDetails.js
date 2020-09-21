9a0690271a1866ac7684b07e139e8094d1530b0d
eunsu-lim <limes1787@gmail.com>
2020-09-21 17:08:31 +0900

    [12기 임은수] Refactor : 상품상세페이지 완료

--- src/Pages/ProductDetails/ProductDetails.js
+++ src/Pages/ProductDetails/ProductDetails.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import Slider from "react-slick";
+import SpecList from "./component/SpecList";
 import ProductImg from "./component/ProductImg";
 import COLOR from "./colorData";
 import PRODUCTCOLOR from "./data";
 import PRODUCTSET from "./setupData";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./ProductDetails.scss";
@@ -92,7 +93,6 @@ class ProductDetails extends Component {
                 <p className="infoPrice">$ 29.99</p>
                 <em>Free shipping on orders over $29.00</em>
               </div>
-
               <div className="infoColor">
                 <p className="color">{imgColor}</p>
                 <ul>
@@ -154,7 +154,6 @@ class ProductDetails extends Component {
         <section className="productImglist">
           <div>
             <h6>Features</h6>
-
             <ul>
               <ProductImg productData={productInfos} />
             </ul>
@@ -185,86 +184,7 @@ class ProductDetails extends Component {
             <h6>SPECS & DETAIL</h6>
             <div className="specList">
               <div>
-                <ul>
-                  <li>
-                    <div>
-                      <h4>DIMENSIONS</h4>
-                      <p>
-                        <strong>Mouse Dimensions</strong>
-                      </p>
-                      <p>Height x Width x Depth:</p>
-                      <p>4.21 in (107mm) x 2.32 in (59mm) 1.04 in (26.5mm)</p>
-                      <p>Weight (with batteries): 3.53 oz (100 g)</p>
-                      <p>
-                        <strong>Nano receiver</strong>
-                      </p>
-                      <p>Height x Width x Depth:</p>
-                      <p>
-                        0.74 in (18.7 mm) 0.57 in (14.4 mm) x 0.24 in (6.1 mm)
-                      </p>
-                      <p>Weight: 0.06 oz (1.8 g)</p>
-                      <h4>SYSTEM REQUIREMENTS</h4>
-                      <p>
-                        Windows® | Mac | Chrome® OS | Linux® | Works with
-                        Surface™
-                      </p>
-                      <p>
-                        <strong>Receiver</strong>
-                      </p>
-                      <p>Required: Available USB port</p>
-                      <p>Windows 10 or later, Windows 8, Windows 7</p>
-                      <p>macOS 10.10 or later</p>
-                      <p>Chrome OS</p>
-                      <p>Linux Kernel 2.6+</p>
-                      <p>
-                        <strong>Bluetooth</strong>
-                      </p>
-                      <p>Required: Bluetooth Low Energy technology</p>
-                      <p>Windows 10 or later, Windows 8</p>
-                      <p>macOS 10.10 or later</p>
-                      <p>Chrome OS, Android™ 5.0 or later</p>
-                    </div>
-                  </li>
-                  <li>
-                    <div>
-                      <h4>DIMENSIONS</h4>
-                      <p>
-                        <strong>Mouse Dimensions</strong>
-                      </p>
-                      <p>Height x Width x Depth:</p>
-                      <p>4.21 in (107mm) x 2.32 in (59mm) 1.04 in (26.5mm)</p>
-                      <p>Weight (with batteries): 3.53 oz (100 g)</p>
-                      <p>
-                        <strong>Nano receiver</strong>
-                      </p>
-                      <p>Height x Width x Depth:</p>
-                      <p>
-                        0.74 in (18.7 mm) 0.57 in (14.4 mm) x 0.24 in (6.1 mm)
-                      </p>
-                      <p>Weight: 0.06 oz (1.8 g)</p>
-                      <h4>SYSTEM REQUIREMENTS</h4>
-                      <p>
-                        Windows® | Mac | Chrome® OS | Linux® | Works with
-                        Surface™
-                      </p>
-                      <p>
-                        <strong>Receiver</strong>
-                      </p>
-                      <p>Required: Available USB port</p>
-                      <p>Windows 10 or later, Windows 8, Windows 7</p>
-                      <p>macOS 10.10 or later</p>
-                      <p>Chrome OS</p>
-                      <p>Linux Kernel 2.6+</p>
-                      <p>
-                        <strong>Bluetooth</strong>
-                      </p>
-                      <p>Required: Bluetooth Low Energy technology</p>
-                      <p>Windows 10 or later, Windows 8</p>
-                      <p>macOS 10.10 or later</p>
-                      <p>Chrome OS, Android™ 5.0 or later</p>
-                    </div>
-                  </li>
-                </ul>
+                <SpecList />
               </div>
             </div>
             <div className="productSupport">
