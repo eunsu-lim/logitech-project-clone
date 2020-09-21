400c5d60f700e739998ba5ddf0336a2f196cafce
yechanHong <h19960626@gmail.com>
2020-09-21 11:57:31 +0900

    Refactor: 충돌 해결

--- src/Routes.js
+++ src/Routes.js
@@ -7,22 +7,21 @@ import ProductDetails from "./Pages/ProductDetails/ProductDetails";
 import ProductList from "./Pages/ProductList/ProductList";
 import SignUp from "./Pages/SignUp/SignUp";
 
-class Routes extends React.Component { 
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path="/Account" component={Account} />
-                    <Route exact path="/Login" component={Login} />
-                    <Route exact path="/Main" component={Main} />
-                    <Route exact path="/ProductDetails" component={ProductDetails} />
-                    <Route exact path="/ProductList" component={ProductList} />
-                    <Route exact path="/SignUp" component={SignUp} />
-                </Switch>
-            </Router>
-        );
-    }
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/Account" component={Account} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/Main" component={Main} />
+          <Route exact path="/ProductDetails" component={ProductDetails} />
+          <Route exact path="/ProductList" component={ProductList} />
+          <Route exact path="/SignUp" component={SignUp} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default Routes;
-
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
-import "../src/Styles/common.scss"
 import "../src/Styles/reset.scss";
- 
+import "../src/Styles/common.scss";
+
 ReactDOM.render(
   <React.StrictMode>
     <Routes />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
