188280b626d01419f099888f548da343451efff7
eunsu-lim <limes1787@gmail.com>
2020-09-21 17:14:46 +0900

    [12기 임은수] Add : 장바구니 페이지 셋팅 수정

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Signup extends Component {
-    render() {
-        return (
-            <div>
-
-            </div>
-        );
-    }
+  render() {
+    return <div></div>;
+  }
 }
 
 export default Signup;
