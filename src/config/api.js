6ac6eefcd8138b9b1f990ff8c9d7d10bbf0d2656
ko jae won <sis9410@gmail.com>
2020-09-25 13:30:16 +0900

    api.js 수정

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-export const api = "http://10.58.1.236:8000";
+export const api = "http://10.58.5.137:800";
 
-export const LoginAPI = "http://10.58.1.236:8000";
+export const LoginAPI = "http://10.58.5.137:800";
